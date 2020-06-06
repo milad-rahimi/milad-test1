7902fd3ef69e54c0a0f9bbcd0f96e921c4ddb660
milad <milad@yahoo.com>
2020-06-06 20:44:34 +0430

    we are on mac

---
+++ web/models.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Expense(models.Model):
+    text = models.CharField(max_length=225)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=225)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
